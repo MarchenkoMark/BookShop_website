c1e6aa9a457b292bcda7732ecc7bf4578075c070
Mark <50804946+MarchenkoMark@users.noreply.github.com>
2020-07-27 12:04:07 +0300

    Add files via upload

---
+++ Shop_site/js/login.js
@@ -0,0 +1,57 @@
+var isLogged = false;
+
+var objPeople = [
+    {
+        username: "mark",
+        password: "mark"
+    },
+    {
+        username: "user1",
+        password: "12345"
+    },
+    {
+        username: "user2",
+        password: "12345"
+    },
+    {
+        username: "admin",
+        password: "87654321"
+    },
+
+]
+
+function getInfo() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    objPeople.forEach(element => {
+        if(username == element.username && password == element.password) {
+            console.log(username + " logged in");
+            document.getElementById("message").innerHTML = "You're logged";
+            login();
+            return;
+        }
+    });
+    if(!isLogged) document.getElementById("message").innerHTML = "Incorrect username or password";
+}
+
+function checkLogged() {
+    console.log("checklogged")
+    if(!isLogged){
+        document.getElementById('modal').classList.add('active')
+    } 
+    else {
+        window.location.replace('profile.html')
+    }
+}
+
+function login() {
+    console.log("login");
+    isLogged = true;
+    document.getElementById('btn').textContent = "log out"
+}
+function logout() {
+    console.log("logout");
+    isLogged = false;
+    document.getElementById('btn').textContent = "log in"
+}
---
+++ Shop_site/js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+$(function(){
+    $('.slider__inner').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: 'slick-dots',
+        focusOnSelect: false,
+    });
+}); 
+
